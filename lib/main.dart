bd5473308a4892a4aab30e7c4a80226a703a8992
MarkRyuo <malupajhonmark3035@gmail.com>
2024-07-05 10:28:35 +0800

    update herE

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,13 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Text('Hello, Niyari'),
+    home: Scaffold(
+      appBar: AppBar(
+        title: const ('My Coffee'),
+        backgroundColor: Color.fromARGB(255, 240, 222, 169),
+        
+      ),
+    )
   ));
 }
 
