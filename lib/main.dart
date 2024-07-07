351c71af5c85be2d5129ce74b1e44afe003f5cba
MarkRyuo <malupajhonmark3035@gmail.com>
2024-07-07 18:10:58 +0800

    update here

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,6 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: const Text('My Coffee'),
-        backgroundColor: Color.fromARGB(255, 240, 222, 169),
-        
-      ),
-    )
-  ));
+  runApp(();
 }
 
