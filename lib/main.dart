2a1e769f2f04b5168b963790bfb437080cb5a55d
MarkRyuo <malupajhonmark3035@gmail.com>
2024-07-07 18:14:30 +0800

    update here

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MaterialApp(
     home : Scaffold(
-      
+      appBar: AppBar(
+        title: Text("Coffee Shop"),
+      ),
     ),
   ));
 }
